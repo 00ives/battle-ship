fa6395e43975b8e4aecf752c103f7ce26eb79289
Ivor Lewis <ivor.lewis@gmail.com>
2025-05-10 23:15:42 -0600

    code from where i left off last time

--- src/app.svelte
+++ src/app.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
   import GameboardContainer from '$lib/components/gameboard/GameboardContainer.svelte';
   import Setup from '$lib/components/Setup/setup.svelte';
+  import ShipSetup from '$lib/components/Setup/ship-setup/ShipSetup.svelte';
   import { useApp } from '$lib/providers/app';
-//test
+  //test
   const { setupService } = useApp().services;
 </script>
 
 <Setup />
-
-
-<GameboardContainer
-  numberOfShips={2}
-  shipConfig={[
-    { id: 1, x: 'a', y: '1', direction: 'horizontal' },
-    { id: 2, x: 'a', y: '1', direction: 'vertical' },
-  ]}
-/>
+<div>
+  <ShipSetup />
+  <GameboardContainer
+    numberOfShips={2}
+    shipConfig={[
+      { id: 1, x: 'a', y: '1', direction: 'horizontal' },
+      { id: 2, x: 'a', y: '1', direction: 'vertical' },
+    ]}
+  />
+</div>
